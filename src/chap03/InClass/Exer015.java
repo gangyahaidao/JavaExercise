be5fcdcf4aad52c05c1372a1179a8539dcde960b
gangyahaidao <736791342@qq.com>
2020-03-03 11:54:31 +0800

    增加300例练习

---
+++ src/chap03/InClass/Exer015.java
@@ -0,0 +1,30 @@
+package chap03.InClass;
+
+import java.util.Scanner;
+
+public class Exer015 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("请输入一个加密原始字符串：");
+		String password = scan.nextLine();
+		char[] array = password.toCharArray();
+		for(int i = 0; i < array.length; i++) {
+			array[i] = (char) (array[i]^20000);
+		}
+		System.out.println("加密结果如下：" + new String(array));
+		
+		System.out.println("开始进行解密：");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		for(int i = 0; i < array.length; i++) {
+			array[i] = (char) (array[i]^20000);
+		}
+		System.out.println("解密结果如下：" + new String(array));
+	}
+
+}
